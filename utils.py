cc3e13f63892f2d557f8a29a630bbf98a8e47de1
Andreas <60923027+starheim98@users.noreply.github.com>
2023-10-31 13:09:09 +0100

    util

--- utils.py
+++ utils.py
@@ -56,42 +56,47 @@ def merge_train_target(train_df, target_df):
     merged_df = target_df.merge(train_df, left_on='time', right_on='date_forecast')
     return merged_df
 
-def clean_data(target_df, estimated_df, observed_df, zero_threshold=200, constant_value_threshold=1):
+def clean_and_plot_target_data(target_df, zero_threshold=150, constant_value_threshold=1):
     """
-    Clean the datasets by removing specified patterns, using the "time" column as the ID.
+    Clean the target dataset by removing specified patterns and plot before and after cleaning.
 
     Parameters:
     - target_df: DataFrame containing the target values.
-    - estimated_df, observed_df, test_df: DataFrames to be cleaned based on the target DataFrame.
     - zero_threshold: The threshold for consecutive zero values to be removed.
     - constant_value_threshold: The threshold for consecutive constant non-zero values to be removed.
 
     Returns:
-    - Cleaned target, estimated, observed, and test DataFrames.
+    - Cleaned target DataFrame.
     """
     
+    # Working on a copy to avoid modifying the original DataFrame
+    target_df = target_df.copy()
+
+    target_df = target_df.dropna(subset=['pv_measurement'])
+
     # Identify sequences of zeros in the target
     zero_sequences = (target_df['pv_measurement'] == 0).astype(int).groupby(target_df['pv_measurement'].ne(0).cumsum()).cumsum()
 
     # Identify sequences of constant non-zero values in the target
-    constant_sequences = target_df['pv_measurement'].ne(target_df['pv_measurement'].shift()).groupby(target_df['pv_measurement']).cumsum()
+    constant_sequences = (target_df['pv_measurement'].eq(target_df['pv_measurement'].shift()) & (target_df['pv_measurement'] != 0)).astype(int).groupby(target_df['pv_measurement'].ne(target_df['pv_measurement'].shift()).cumsum()).cumsum()
 
     # Mark rows to be removed
     remove_zeros = zero_sequences > zero_threshold
-    remove_constants = target_df['pv_measurement'].duplicated(keep=False) & (target_df['pv_measurement'] != 0)
+    remove_constants = constant_sequences > constant_value_threshold
 
     # Combine the conditions
     to_remove = remove_zeros | remove_constants
 
-    # Extract the "time" IDs of the rows to be removed
-    time_ids_to_remove = target_df.loc[to_remove, 'time']
+    total_data_points = len(target_df)
+    removed_data_points = to_remove.sum()
 
-    # Remove marked rows from all DataFrames based on the "time" IDs
-    cleaned_target = target_df[~target_df['time'].isin(time_ids_to_remove)].reset_index(drop=True)
-    cleaned_estimated = estimated_df[~estimated_df['time'].isin(time_ids_to_remove)].reset_index(drop=True)
-    cleaned_observed = observed_df[~observed_df['time'].isin(time_ids_to_remove)].reset_index(drop=True)
+    print(f"Total data points: {total_data_points}")
+    print(f"Data points to be removed: {removed_data_points}")
 
-    return cleaned_target, cleaned_estimated, cleaned_observed
+    # Clean the target data
+    cleaned_target = target_df[~to_remove].reset_index(drop=True)
+
+    return cleaned_target
 
 def preprocess_category(category: str):
     category = category.upper()
@@ -99,17 +104,23 @@ def preprocess_category(category: str):
     target_df = pd.read_parquet(f'data/{category}/train_targets.parquet')
     estimated_df = pd.read_parquet(f'data/{category}/X_train_estimated.parquet')
     observed_df = pd.read_parquet(f'data/{category}/X_train_observed.parquet')
+    test_df = pd.read_parquet(f'data/{category}/X_test_estimated.parquet')
 
-    cleaned_target, cleaned_estimated, cleaned_observed = clean_data(target_df=target_df, estimated_df=estimated_df,
-                                                                     observed_df=observed_df)
+    cleaned_target = clean_and_plot_target_data(target_df=target_df)
 
-    preprocessed_df = concat_observed_estimated(cleaned_observed, cleaned_estimated)
+    preprocessed_df = concat_observed_estimated(observed_df, estimated_df)
     preprocessed_df = onehot_hours(preprocessed_df)
     preprocessed_df = onehot_months(preprocessed_df)
+
+    preprocessed_test = onehot_hours(test_df)
+    preprocessed_test = onehot_months(preprocessed_test)
     
     target_interpolated = interpolate_quarter_hourly(cleaned_target)
 
     merged_df = merge_train_target(preprocessed_df, target_interpolated)
+    merged_df.fillna(0, inplace=True)
 
-    return merged_df
+
+    return merged_df, preprocessed_test
+
 
